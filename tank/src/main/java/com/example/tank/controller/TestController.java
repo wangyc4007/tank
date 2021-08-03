b3292417a7df9bbd8ba4ce0bbc19a53bf5bbff59
wangyc4007 <m18059234007@163.com>
2021-08-03 22:52:24 +0800

    测试

--- tank/src/main/java/com/example/tank/controller/TestController.java
+++ tank/src/main/java/com/example/tank/controller/TestController.java
@@ -1,8 +1,20 @@
 package com.example.tank.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Y~chao
  * @create 2021/8/3 15:58
  */
+@RestController
+@RequestMapping("/test/")
 public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "hello world！！！";
+    }
+
 }
